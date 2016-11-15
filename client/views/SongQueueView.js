b925517afab1eb94d27d74d9eb20318a20fe0639
dzheng93 <dunstanzheng@gmail.com>
2016-11-14 20:06:19 -0800

    Pass half of the SpecRunner tests

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,9 +2,17 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.collection.on('add', function() {
+      this.render();
+    }, this);
+
+    this.collection.on('pop', function() {
+      this.render();
+    }, this);
   },
 
   render: function() {
+    SongQueueEntryView.prototype.render.call(this);
     return this.$el;
   }
 
